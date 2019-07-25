e325652df0ac8be5f0e54d6c3e03f62eb3704850
Gabe <jflores353@gmail.com>
2019-07-25 13:17:32 -0500

    gave rulees to loading wheel for when to load, also hide form after submit

--- client.js
+++ client.js
@@ -1,8 +1,11 @@
 const form = document.querySelector("form");
+const loadingElement = document.querySelector(".loading");
+
+loadingElement.style.display = "none";
 
 form.addEventListener("submit", e => {
   e.preventDefault();
-  const formData = new FormData(form);
+  const formData = new FormData(form); // * formData is built into the browser, it works by passing in reference to the form
   const name = formData.get("name");
   const content = formData.get("content");
 
@@ -12,4 +15,6 @@ form.addEventListener("submit", e => {
   };
 
   console.log(post);
+  form.style.display = "none"; // * now after submit the form will not display
+  loadingElement.style.display = ""; //* loading wheel will now display briefly after submit
 });
